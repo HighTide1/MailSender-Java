a628aa7db25031ad6d51a739519a5ac733807123
HighTide <jeredt1@gmail.com>
2015-02-09 22:55:27 -0500

    Added Encryption/Decryption Utilities in SecurityUtilities.
    Signed-off-by: HighTide <jeredt1@gmail.com>

--- src/main/java/com/hightide/mailsender/util/SecurityUtilities.java
+++ src/main/java/com/hightide/mailsender/util/SecurityUtilities.java
@@ -23,6 +23,28 @@
 
 package com.hightide.mailsender.util;
 
+import javax.annotation.Nonnull;
+
+import javax.swing.JOptionPane;
+
+import java.awt.HeadlessException;
+
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import java.security.spec.InvalidKeySpecException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
+
 /**
  * Utility class for the Encryption/Decryption of .cfg
  * 
@@ -30,6 +52,172 @@ package com.hightide.mailsender.util;
  * @author Jered Tupik
  *
  */
-public class SecurityUtilities {
-
+public class SecurityUtilities{
+	
+	/**
+	 * PBEKeySpec used for the generation
+	 * of a password-based key
+	 */
+	private static PBEKeySpec 			PBEKeySpecification;
+	
+	/** 
+	 * PBEParameterSpec used for the generation
+	 * a salt/count based specification
+	 */
+	private static PBEParameterSpec     PBEParameterSpecification;
+	
+	/**
+	 * Secret Key for PBE Encryption/Decryption
+	 */
+	private static SecretKey			PBEKey;
+	
+	/**
+	 * SecretKeyFactory for the PBE Encryption/Decryption
+	 */
+	private static SecretKeyFactory		PBESecretKeyGenerator;
+	
+	/**
+	 * PBECipher used for encryption/decryption of 
+	 * files
+	 */
+	private static Cipher 				PBECipher;
+	
+	/**
+	 * Salt used for the PBEParameterSpecification.
+	 * NOTE: If someone other than the author is
+	 * using this program, CHANGE THESE VALUES.
+	 * Otherwise, the security of your files
+	 * cannot be guaranteed.
+	 */
+	private static byte[] SALT = {
+		(byte)0x7F, (byte)0x09, (byte)0xFF, (byte)0xAD,
+		(byte)0xA8, (byte)0x00, (byte)0xCF, (byte)0x9A
+	};
+	
+	/**
+	 * Count used for the PBEParameterSpecification
+	 * NOTE: If someone other than the author is
+	 * using this program, CHANGE THESE VALUES.
+	 * Otherwise, the security of your files
+	 * cannot be guaranteed.
+	 */
+	private static int ITER_COUNT = 10;
+	
+	//public static void setupSecurityUtilities(){
+	static{
+		try{
+			PBESecretKeyGenerator = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
+		}catch(NoSuchAlgorithmException NSA){
+			System.err.println(NSA);
+			System.exit(1);
+		}
+		
+		PBEParameterSpecification = new PBEParameterSpec(SALT, ITER_COUNT);
+		
+		try{
+			PBECipher = Cipher.getInstance("PBEWithMD5AndDES");
+		}catch(Exception E){
+			System.err.println(E);
+			System.exit(1);
+		}
+	}
+	
+	/**
+	 * Encryptes the text with a user-given password and salt
+	 * 
+	 * @return The Encrypted Text
+	 */
+	public static byte[] encryptText(){
+		byte[] 		  UserInput;
+		StringBuilder encryptedText = new StringBuilder("");
+		byte[]   	  encryptedBytes = new byte[0];
+		String 		  Password = "";
+		
+		//setupSecurityUtilities();
+		
+		Password = JOptionPane.showInputDialog(null, "Please Enter the Encryption Password.", 
+				                               "PROMPT", JOptionPane.PLAIN_MESSAGE);
+		PBEKeySpecification = new PBEKeySpec(Password.toCharArray());
+		
+		try{
+			PBEKey = PBESecretKeyGenerator.generateSecret(PBEKeySpecification);
+		}catch(InvalidKeySpecException IKS){
+			System.err.println(IKS);
+			System.exit(1);
+		}
+		
+		try {
+			PBECipher.init(Cipher.ENCRYPT_MODE, PBEKey, PBEParameterSpecification);
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidAlgorithmParameterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			encryptedBytes = PBECipher.doFinal(JOptionPane.showInputDialog(null, "Please Enter the text to encrypt.", 
+					                                "PROMPT", JOptionPane.PLAIN_MESSAGE).getBytes());
+		} catch (HeadlessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return encryptedBytes;
+	}
+	
+	/**
+	 * Decrypts the text with a user-given password and encrypted text
+	 * 
+	 * @param encryptedBytes The encrypted bytes to decrypt
+	 * @return The decrypted text
+	 */
+	public static byte[] decryptText(@Nonnull byte[] encryptedBytes){
+		byte[] 		  UserInput;
+		StringBuilder decryptedText = new StringBuilder("");
+		byte[]   	  decryptedBytes = new byte[0];
+		String 		  Password = "";
+		
+		//setupSecurityUtilities();
+		
+		Password = JOptionPane.showInputDialog(null, "Please Enter the Encryption Password.", 
+				                               "PROMPT", JOptionPane.PLAIN_MESSAGE);
+		PBEKeySpecification = new PBEKeySpec(Password.toCharArray());
+		
+		try{
+			PBEKey = PBESecretKeyGenerator.generateSecret(PBEKeySpecification);
+		}catch(InvalidKeySpecException IKS){
+			System.err.println(IKS);
+			System.exit(1);
+		}
+		
+		try {
+			PBECipher.init(Cipher.DECRYPT_MODE, PBEKey, PBEParameterSpecification);
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidAlgorithmParameterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			decryptedBytes = PBECipher.doFinal(encryptedBytes);
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return decryptedBytes;
+	}
 }
